a19134e99a663f86c2df38e41e58a73d6376cb13
3bsa <alemubekele923@gmail.com>
2020-12-29 22:37:15 +0300

    Update shouldCompress.js

--- src/shouldCompress.js
+++ src/shouldCompress.js
@@ -2,13 +2,13 @@ const MIN_COMPRESS_LENGTH = 10240
 const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 10
 
 function shouldCompress(req) {
-  const { originType, originSize, webp } = req.params
+  const { originType, originSize, avif } = req.params
 
   if (!originType.startsWith('image')) return false
   if (originSize === 0) return false
   if (webp && originSize < MIN_COMPRESS_LENGTH) return false
   if (
-    !webp &&
+    !avif &&
     (originType.endsWith('png') || originType.endsWith('gif')) &&
     originSize < MIN_TRANSPARENT_COMPRESS_LENGTH
   ) {
